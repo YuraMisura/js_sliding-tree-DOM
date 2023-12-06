ec6904a4e5c143e925226a290fc8652878a90e38
Юрий Мисюра <apriory13@meta.ua>
2023-12-06 15:04:13 +0200

    make build

---
+++ index.331ea836.js
@@ -0,0 +1,3 @@
+var e=document.querySelector(".tree");document.querySelectorAll(".tree li").forEach(function(e){var t=document.createElement("span"),r=e.firstChild;e.prepend(t),t.append(r)}),e.addEventListener("click",function(e){var t=e.target;if("SPAN"===t.tagName){var r=t.closest("li").querySelector("ul");r&&(r.hidden=!r.hidden)}});//# sourceMappingURL=index.331ea836.js.map
+
+//# sourceMappingURL=index.331ea836.js.map
